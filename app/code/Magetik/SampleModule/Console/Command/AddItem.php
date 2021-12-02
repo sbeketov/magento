e89e922e5b847b1f0b1989cfee2b920efd0bea37
Segrey Beketov <>
2021-12-02 14:26:20 +0300

    SampleModule add console command

---
+++ app/code/Magetik/SampleModule/Console/Command/AddItem.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Magetik\SampleModule\Console\Command;
+
+use Magento\Framework\Console\Cli;
+use Symfony\Component\Console\Command\Command;
+use Magetik\SampleModule\Model\ItemFactory;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class AddItem extends Command
+{
+    const INPUT_KEY_NAME = 'name';
+    const INPUT_KEY_DESCRIPTION = 'description';
+    /**
+     * @var ItemFactory $itemFactory
+     */
+    private  $itemFactory;
+
+    public function __construct(ItemFactory $itemFactory)
+    {
+        $this->itemFactory = $itemFactory;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('magetik:item:add')
+            ->addArgument(
+                self::INPUT_KEY_NAME,
+                InputArgument::REQUIRED,
+                'Item name'
+            )->addArgument(
+                self::INPUT_KEY_DESCRIPTION,
+                InputArgument::OPTIONAL,
+                'Item description'
+            );
+
+        parent::configure();
+    }
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $item = $this->itemFactory->create();
+        $item->setName($input->getArgument(self::INPUT_KEY_NAME));
+        $item->setDescription($input->getArgument(self::INPUT_KEY_DESCRIPTION));
+        $item->setIsObjectNew(true);
+        $item->save();
+        return Cli::RETURN_SUCCESS;
+    }
+}
